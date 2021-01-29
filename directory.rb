0a078f56738c010201f41335ed50b6c7a97151c3
kasey <kasey.purvor@gmail.com>
2021-01-29 11:15:34 +0000

    Step11 saving student data to csv file, using .gitignore file to store data

--- directory.rb
+++ directory.rb
@@ -1,14 +1,13 @@
+@students = []
+
 def input_students
-  puts "Please input student names"
-  puts "To finish entering names, enter empty name value"  
-  students = []
+  puts "Please input student names. \n To finish entering names, enter 'stop'"  
   name = gets.chomp
-  until name.empty?
-    students << {name: name, cohort: :november}
-    puts "We now have #{students.length} students."
+  until name == "stop"
+    @students << {name: name, cohort: :november}
+    puts "We now have #{@students.length} students."
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -26,25 +25,46 @@ def print_footer(student_arr)
   puts "Overall, we have #{student_arr.length} great students"
 end
 
+def print_menue
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save students"
+  puts "9. Exit the program"
+end
+
+def process(command)
+  case command
+    when "1" 
+      input_students
+    when "2"
+      print_header
+      print_names(@students)
+      print_footer(@students)
+    when "3"
+      save_students 
+    when "9" 
+      exit
+    else
+        puts "Unrecognised command, please try again"  
+  end 
+end
+
+def save_students
+  file = File.open("students.csv", "w") 
+  @students.each { |student_hsh| 
+    student_data = [student_hsh[:name], student_hsh[:cohort]]
+    student_csv_line = student_data.join(",")
+    file.puts student_csv_line
+  }
+  file.close
+end
+  
+  
+  
 def interactive_menue
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit the program"
-    command = gets.chomp
-    case command
-      when "1" 
-        students += input_students
-      when "2"
-        print_header
-        print_names(students)
-        print_footer(students)
-      when "9" 
-        break
-      else
-        puts "Unrecognised command, please try again" 
-    end
+    print_menue
+    process(gets.chomp)    
   end
 end
 
